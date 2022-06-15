a0826a7c8d11271c0e9a0b5f4e19fe44b34944ee
ilyailyushko <65430523+ilyailyushko@users.noreply.github.com>
2022-06-16 00:37:15 +0300

    Добавлен метод для изменения задачи

--- main.py
+++ main.py
@@ -23,11 +23,46 @@ app.mount('/flsk', WSGIMiddleware(flask_app))
 # Секция Flask
 
 @flask_app.route('/ontask/', methods = ['POST'])
-def comment_to_entity():
+def get_task_uf_field():
+    """"
+    Метод для подписки на создание / изменение задачи
+    """
     r = dict(request.form)
-    print(r)
+    task_id = r['data[FIELDS_AFTER][ID]']
+    print('Создана задача, id', task_id)
+    requests.post(f'http://194.67.119.72/tasks_task_update_by_uf_field/?task_id={task_id}')
+
     return 'OK'
 
 @app.get("/")
 def read_root():
     return {"ghbdtn": "MIR"}
+
+@app.post('/tasks_task_getFields/')
+def tasks_task_getFields():
+    '''
+    ## Метод для получения полей задачи
+    '''
+    task_fields = b.call('tasks.task.getFields', raw=True) 
+    return task_fields
+
+
+@app.post('/tasks_task_update_by_uf_field/')
+def tasks_task_get_uf_value(task_id):
+    """ 
+    # Обновление задачи по содержанию кастомного поля
+    Метод получает значение пользовательского поля задачи,  если оно заполненно, добавляет в задачу префикс и соответсвующий тег. 
+    """
+    get_task = b.call('tasks.task.get', {'taskId': task_id, 'select': ['UF_AUTO_395874465748', "TITLE", "DESCRIPTION"]})
+    task = get_task['task']
+    title = task['title']
+    description = task['description']
+    uf_field = task['ufAuto395874465748'] 
+    print(task)
+    if task['ufAuto395874465748']:
+        title = f"[{uf_field}] {title}"
+        description = f"{description} \n\n #{uf_field}"
+        b.call('tasks.task.update', {'taskId': task['id'], 'fields': {"TITLE": title, "DESCRIPTION": description}})
+        return task['id'], 'задача обновлена'
+    else:
+        return 'пользовательское поле задачи не заполненно'
